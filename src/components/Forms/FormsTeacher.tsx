26f338f50bae5369b957b6d27a560188748d4944
bmalkes <bmalkes@gmail.com>
2024-01-14 18:35:24 +0200

    feat:add sidebar and react hook form

--- src/components/Forms/FormsTeacher.tsx
+++ src/components/Forms/FormsTeacher.tsx
@@ -1,14 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+
+const schemaForm = z.object({
+  firstName: z.string({
+    required_error: "First name is required",
+    invalid_type_error: "First name must be a string",
+  }),
+  lastName: z.string({
+    required_error: "Lastname is required",
+    invalid_type_error: "Lastname must be a string",
+  }),
+
+  nameOfKind: z.string(),
+  city: z.string(),
+  Hours: z.string(),
+  Adress: z.string({
+    required_error: "Adress is required",
+  }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(5, "please the password must need to be at more 5 than 5 caracters")
+    .max(20, "Please the password must be at least 20 characters"),
+});
 
 const FormsTeacher = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schemaForm),
+  });
   const navigate = useNavigate();
 
   const handleKindeGardenForm = (data: any) => {
     console.log(data);
   };
-
+  console.log(errors);
   return (
     <form onSubmit={handleSubmit(handleKindeGardenForm)}>
       <div className="flex gap-2 ">
@@ -18,7 +49,7 @@ const FormsTeacher = () => {
             type="text"
             className="border-2 rounded-md p-1 w-36 md:w-full"
             placeholder="Name"
-            {...register("name", { required: true })}
+            {...register("firstName", { required: true })}
           />
         </div>
         <div className="flex flex-col gap-2 flex-1">
@@ -27,7 +58,7 @@ const FormsTeacher = () => {
             type="text"
             className="border-2 rounded-md p-1 w-36 md:w-full"
             placeholder="LastName"
-            {...register("lastname", { required: true })}
+            {...register("lastName", { required: true })}
           />
         </div>
       </div>
@@ -62,7 +93,7 @@ const FormsTeacher = () => {
             type="text"
             className="border-2 rounded-md p-1 w-36 md:w-full"
             placeholder="Residence"
-            {...register("Residence", { required: true })}
+            {...register("nameOfKind", { required: true })}
           />
         </div>
         <div className="flex flex-col gap-2 flex-1">
@@ -71,7 +102,7 @@ const FormsTeacher = () => {
             type="text"
             className="border-2 rounded-md p-1 w-36 md:w-full"
             placeholder="adress"
-            {...register("adress", { required: true })}
+            {...register("Adress", { required: true })}
           />
         </div>
       </div>
@@ -82,7 +113,7 @@ const FormsTeacher = () => {
             className="border-2 rounded-md p-1 w-36 md:w-full"
             type="time"
             placeholder="Open Hours"
-            {...register("hours", { required: true })}
+            {...register("Hours", { required: true })}
           />
         </div>
         <div className="flex flex-col gap-2 flex-1">
@@ -91,7 +122,7 @@ const FormsTeacher = () => {
             className="border-2 rounded-md p-1 w-36 md:w-full"
             type="Text"
             placeholder="City of the residence"
-            {...register("citie", { required: true })}
+            {...register("city", { required: true })}
           />
         </div>
       </div>
