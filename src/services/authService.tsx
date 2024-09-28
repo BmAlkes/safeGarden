61ca51295725ae188bee6ca4f3e20701001358ad
bmalkes <bmalkes@gmail.com>
2024-09-28 20:18:47 +0300

    git add credentials

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -3,6 +3,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,  // Ensures credentials like cookies are sent
+  headers: {
+      'Content-Type': 'application/json'
+  }
 });
 
 axios.defaults.withCredentials = true;
